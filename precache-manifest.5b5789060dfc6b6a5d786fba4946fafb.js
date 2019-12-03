eb76c540ca04ca4c720ca0e958b0cf59d37aaf55
rhsiun <rhsiun@gmail.com>
2019-12-03 02:09:24 -0600

    Updates

---
+++ precache-manifest.5b5789060dfc6b6a5d786fba4946fafb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "047b108f92e9aa1535e7",
+    "url": "/09-todo-react-rhsiun/static/css/main.7829d680.chunk.css"
+  },
+  {
+    "revision": "047b108f92e9aa1535e7",
+    "url": "/09-todo-react-rhsiun/static/js/main.047b108f.chunk.js"
+  },
+  {
+    "revision": "3b8df68dc72450205b31",
+    "url": "/09-todo-react-rhsiun/static/js/1.3b8df68d.chunk.js"
+  },
+  {
+    "revision": "6d3ac4c1d54bc521a17e",
+    "url": "/09-todo-react-rhsiun/static/js/runtime~main.6d3ac4c1.js"
+  },
+  {
+    "revision": "e5f10838797044a1762191902c3b52a2",
+    "url": "/09-todo-react-rhsiun/index.html"
+  }
+];
